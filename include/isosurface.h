f4a54ee23814486eec392409f93df68e26d2e286
Luis Kao <kogiokkafrms@gmail.com>
2020-03-31 08:30:47 +0800

    Code improvement.

--- include/isosurface.h
+++ include/isosurface.h
@@ -17,7 +17,7 @@ class Isosurface
   float const h_;
   unsigned int target_value_;
   unsigned int vertex_count_;
-  std::vector<int> dimensions_;
+  std::array<int, 3> dimensions_;
   std::vector<unsigned char> isovalues_;
   std::vector<glm::vec3> gradients_;
   std::vector<float> render_data_;
@@ -27,12 +27,12 @@ class Isosurface
     int x_;
     int y_;
     int z_;
-    std::vector<glm::vec3> voxel_index_;
+    std::array<glm::vec3, 8> voxel_index_;
 
   public:
     GridCell(int x, int y, int z);
     ~GridCell();
-    glm::vec3 const& VoxelIndex(int index) const;
+    glm::vec3 const& operator[](int index) const;
   };
 
 public:
@@ -46,14 +46,18 @@ public:
   unsigned int RenderVertexCount() const;
 
 private:
-  glm::vec3 InterpolatedVertex(glm::vec3 const& idx1, glm::vec3 const& idx2);
-  glm::vec3 InterpolatedNormal(glm::vec3 const& idx1, glm::vec3 const& idx2);
+  glm::vec3 InterpolatedVertex(glm::vec3 const& v1, glm::vec3 const& v2);
+  glm::vec3 InterpolatedNormal(glm::vec3 const& v1, glm::vec3 const& v2);
 
 private:
-  inline float CenteredDifference(unsigned short front, unsigned short back) const;
-  inline float ForwardDifference(unsigned short self, unsigned short front) const;
-  inline float BackwardDifference(unsigned short self, unsigned short back) const;
+  inline float CenteredDifference(unsigned short front,
+                                  unsigned short back) const;
+  inline float ForwardDifference(unsigned short self,
+                                 unsigned short front) const;
+  inline float BackwardDifference(unsigned short self,
+                                  unsigned short back) const;
   inline unsigned short Value(int x, int y, int z) const;
+  inline unsigned short Value(glm::vec3 const& v) const;
   inline glm::vec3 const& Gradient(int x, int y, int z) const;
   inline int Index(int x, int y, int z) const;
 };
--- src/isosurface.cc
+++ src/isosurface.cc
@@ -61,24 +61,24 @@ Isosurface::MarchingCube()
   for (int z = 0; z < zsize - 1; ++z) {
     for (int y = 0; y < ysize - 1; ++y) {
       for (int x = 0; x < xsize - 1; ++x) {
-        unsigned short cube_index = 0;
         GridCell cell(x, y, z);
+        unsigned short cube_index = 0;
 
-        if (Value(x, y, z) < target_value_)
+        if (Value(cell[0]) < target_value_)
           cube_index |= 1;
-        if (Value(x + 1, y, z) < target_value_)
+        if (Value(cell[1]) < target_value_)
           cube_index |= 2;
-        if (Value(x + 1, y, z + 1) < target_value_)
+        if (Value(cell[2]) < target_value_)
           cube_index |= 4;
-        if (Value(x, y, z + 1) < target_value_)
+        if (Value(cell[3]) < target_value_)
           cube_index |= 8;
-        if (Value(x, y + 1, z) < target_value_)
+        if (Value(cell[4]) < target_value_)
           cube_index |= 16;
-        if (Value(x + 1, y + 1, z) < target_value_)
+        if (Value(cell[5]) < target_value_)
           cube_index |= 32;
-        if (Value(x + 1, y + 1, z + 1) < target_value_)
+        if (Value(cell[6]) < target_value_)
           cube_index |= 64;
-        if (Value(x, y + 1, z + 1) < target_value_)
+        if (Value(cell[7]) < target_value_)
           cube_index |= 128;
 
         if (table::kEdgeTable.at(cube_index) == 0)
@@ -87,76 +87,52 @@ Isosurface::MarchingCube()
         std::array<glm::vec3, 12> vert_list;
         std::array<glm::vec3, 12> grad_list;
         if (table::kEdgeTable[cube_index] & 1) {
-          glm::vec3 const e1 = cell.VoxelIndex(0);
-          glm::vec3 const e2 = cell.VoxelIndex(1);
-          vert_list[0] = InterpolatedVertex(e1, e2);
-          grad_list[0] = InterpolatedNormal(e1, e2);
+          vert_list[0] = InterpolatedVertex(cell[0], cell[1]);
+          grad_list[0] = InterpolatedNormal(cell[0], cell[1]);
         }
         if (table::kEdgeTable[cube_index] & 2) {
-          glm::vec3 const e1 = cell.VoxelIndex(1);
-          glm::vec3 const e2 = cell.VoxelIndex(2);
-          vert_list[1] = InterpolatedVertex(e1, e2);
-          grad_list[1] = InterpolatedNormal(e1, e2);
+          vert_list[1] = InterpolatedVertex(cell[1], cell[2]);
+          grad_list[1] = InterpolatedNormal(cell[1], cell[2]);
         }
         if (table::kEdgeTable[cube_index] & 4) {
-          glm::vec3 const e1 = cell.VoxelIndex(2);
-          glm::vec3 const e2 = cell.VoxelIndex(3);
-          vert_list[2] = InterpolatedVertex(e1, e2);
-          grad_list[2] = InterpolatedNormal(e1, e2);
+          vert_list[2] = InterpolatedVertex(cell[2], cell[3]);
+          grad_list[2] = InterpolatedNormal(cell[2], cell[3]);
         }
         if (table::kEdgeTable[cube_index] & 8) {
-          glm::vec3 const e1 = cell.VoxelIndex(3);
-          glm::vec3 const e2 = cell.VoxelIndex(0);
-          vert_list[3] = InterpolatedVertex(e1, e2);
-          grad_list[3] = InterpolatedNormal(e1, e2);
+          vert_list[3] = InterpolatedVertex(cell[3], cell[0]);
+          grad_list[3] = InterpolatedNormal(cell[3], cell[0]);
         }
         if (table::kEdgeTable[cube_index] & 16) {
-          glm::vec3 const e1 = cell.VoxelIndex(4);
-          glm::vec3 const e2 = cell.VoxelIndex(5);
-          vert_list[4] = InterpolatedVertex(e1, e2);
-          grad_list[4] = InterpolatedNormal(e1, e2);
+          vert_list[4] = InterpolatedVertex(cell[4], cell[5]);
+          grad_list[4] = InterpolatedNormal(cell[4], cell[5]);
         }
         if (table::kEdgeTable[cube_index] & 32) {
-          glm::vec3 const e1 = cell.VoxelIndex(5);
-          glm::vec3 const e2 = cell.VoxelIndex(6);
-          vert_list[5] = InterpolatedVertex(e1, e2);
-          grad_list[5] = InterpolatedNormal(e1, e2);
+          vert_list[5] = InterpolatedVertex(cell[5], cell[6]);
+          grad_list[5] = InterpolatedNormal(cell[5], cell[6]);
         }
         if (table::kEdgeTable[cube_index] & 64) {
-          glm::vec3 const e2 = cell.VoxelIndex(6);
-          glm::vec3 const e1 = cell.VoxelIndex(7);
-          vert_list[6] = InterpolatedVertex(e1, e2);
-          grad_list[6] = InterpolatedNormal(e1, e2);
+          vert_list[6] = InterpolatedVertex(cell[6], cell[7]);
+          grad_list[6] = InterpolatedNormal(cell[6], cell[7]);
         }
         if (table::kEdgeTable[cube_index] & 128) {
-          glm::vec3 const e1 = cell.VoxelIndex(7);
-          glm::vec3 const e2 = cell.VoxelIndex(4);
-          vert_list[7] = InterpolatedVertex(e1, e2);
-          grad_list[7] = InterpolatedNormal(e1, e2);
+          vert_list[7] = InterpolatedVertex(cell[7], cell[4]);
+          grad_list[7] = InterpolatedNormal(cell[7], cell[4]);
         }
         if (table::kEdgeTable[cube_index] & 256) {
-          glm::vec3 const e1 = cell.VoxelIndex(0);
-          glm::vec3 const e2 = cell.VoxelIndex(4);
-          vert_list[8] = InterpolatedVertex(e1, e2);
-          grad_list[8] = InterpolatedNormal(e1, e2);
+          vert_list[8] = InterpolatedVertex(cell[0], cell[4]);
+          grad_list[8] = InterpolatedNormal(cell[0], cell[4]);
         }
         if (table::kEdgeTable[cube_index] & 512) {
-          glm::vec3 const e1 = cell.VoxelIndex(1);
-          glm::vec3 const e2 = cell.VoxelIndex(5);
-          vert_list[9] = InterpolatedVertex(e1, e2);
-          grad_list[9] = InterpolatedNormal(e1, e2);
+          vert_list[9] = InterpolatedVertex(cell[1], cell[5]);
+          grad_list[9] = InterpolatedNormal(cell[1], cell[5]);
         }
         if (table::kEdgeTable[cube_index] & 1024) {
-          glm::vec3 const e1 = cell.VoxelIndex(2);
-          glm::vec3 const e2 = cell.VoxelIndex(6);
-          vert_list[10] = InterpolatedVertex(e1, e2);
-          grad_list[10] = InterpolatedNormal(e1, e2);
+          vert_list[10] = InterpolatedVertex(cell[2], cell[6]);
+          grad_list[10] = InterpolatedNormal(cell[2], cell[6]);
         }
         if (table::kEdgeTable[cube_index] & 2048) {
-          glm::vec3 const e1 = cell.VoxelIndex(3);
-          glm::vec3 const e2 = cell.VoxelIndex(7);
-          vert_list[11] = InterpolatedVertex(e1, e2);
-          grad_list[11] = InterpolatedNormal(e1, e2);
+          vert_list[11] = InterpolatedVertex(cell[3], cell[7]);
+          grad_list[11] = InterpolatedNormal(cell[3], cell[7]);
         }
 
         for (int i = 0; i < 16; ++i) {
@@ -176,36 +152,38 @@ Isosurface::MarchingCube()
 }
 
 glm::vec3
-Isosurface::InterpolatedVertex(glm::vec3 const& idx1, glm::vec3 const& idx2)
+Isosurface::InterpolatedVertex(glm::vec3 const& v1, glm::vec3 const& v2)
 {
-  float const value1 = Value(idx1.x, idx1.y, idx1.z);
-  float const value2 = Value(idx2.x, idx2.y, idx2.z);
+  float const value1 = Value(v1.x, v1.y, v1.z);
+  float const value2 = Value(v2.x, v2.y, v2.z);
   float const ratio = (target_value_ - value1) / (value2 - value1);
 
   glm::vec3 v;
-  v.x = (idx2.x - idx1.x) * h_ * ratio + idx1.x;
-  v.y = (idx2.y - idx1.y) * h_ * ratio + idx1.y;
-  v.z = (idx2.z - idx1.z) * h_ * ratio + idx1.z;
+  v.x = (v2.x - v1.x) * h_ * ratio + v1.x;
+  v.y = (v2.y - v1.y) * h_ * ratio + v1.y;
+  v.z = (v2.z - v1.z) * h_ * ratio + v1.z;
 
   return v;
 }
 
 glm::vec3
-Isosurface::InterpolatedNormal(glm::vec3 const& idx1, glm::vec3 const& idx2)
+Isosurface::InterpolatedNormal(glm::vec3 const& v1, glm::vec3 const& v2)
 {
-  float const value1 = Value(idx1.x, idx1.y, idx1.z);
-  float const value2 = Value(idx2.x, idx2.y, idx2.z);
+  float const value1 = Value(v1.x, v1.y, v1.z);
+  float const value2 = Value(v2.x, v2.y, v2.z);
   float const ratio = (target_value_ - value1) / (value2 - value1);
 
-  glm::vec3 const& g1 = Gradient(idx1.x, idx1.y, idx1.z);
-  glm::vec3 const& g2 = Gradient(idx2.x, idx2.y, idx2.z);
+  glm::vec3 const& g1 = Gradient(v1.x, v1.y, v1.z);
+  glm::vec3 const& g2 = Gradient(v2.x, v2.y, v2.z);
   return ratio * (g2 - g1) - g1;
 }
 
 void
 Isosurface::ReadRaw(std::string_view const filepath)
 {
-  assert(!dimensions_.empty());
+  assert(dimensions_[0] > 0);
+  assert(dimensions_[1] > 0);
+  assert(dimensions_[2] > 0);
 
   std::ifstream file(filepath.data(), std::ios::binary);
   if (file.fail()) {
@@ -229,6 +207,12 @@ Isosurface::Value(int x, int y, int z) const
   return static_cast<unsigned short>(isovalues_.at(Index(x, y, z)));
 }
 
+inline unsigned short
+Isosurface::Value(glm::vec3 const& v) const
+{
+  return static_cast<unsigned short>(isovalues_.at(Index(v.x, v.y, v.z)));
+}
+
 inline glm::vec3 const&
 Isosurface::Gradient(int x, int y, int z) const
 {
@@ -240,7 +224,6 @@ Isosurface::Index(int x, int y, int z) const
 {
   int const xsize = dimensions_[0];
   int const ysize = dimensions_[1];
-
   return x + y * xsize + z * ysize * xsize;
 }
 
@@ -279,10 +262,12 @@ Isosurface::ReadRawInfo(std::string_view const filepath)
     attrs.push_back(value.substr(n + 1));
   }
 
-  std::stringstream ss(attrs.at(0));
+  std::istringstream ss(attrs.at(0));
   std::string dimen;
+  int index = 0;
   while (std::getline(ss, dimen, ':')) {
-    dimensions_.push_back(std::stoi(dimen));
+    dimensions_[index] = std::stoi(dimen);
+    ++index;
   }
 }
 
@@ -303,22 +288,19 @@ Isosurface::GridCell::GridCell(int x, int y, int z)
   , y_(y)
   , z_(z)
 {
-  voxel_index_.reserve(8);
-
-  voxel_index_.emplace_back(x_, y_, z_);
-  voxel_index_.emplace_back(x_ + 1, y_, z_);
-  voxel_index_.emplace_back(x_ + 1, y_, z_ + 1);
-  voxel_index_.emplace_back(x_, y_, z_ + 1);
-  voxel_index_.emplace_back(x_, y_ + 1, z_);
-  voxel_index_.emplace_back(x_ + 1, y_ + 1, z_);
-  voxel_index_.emplace_back(x_ + 1, y_ + 1, z_ + 1);
-  voxel_index_.emplace_back(x_, y_ + 1, z_ + 1);
+  voxel_index_[0] = glm::vec3{ x_, y_, z_ };
+  voxel_index_[1] = glm::vec3{ x_ + 1, y_, z_ };
+  voxel_index_[2] = glm::vec3{ x_ + 1, y_, z_ + 1 };
+  voxel_index_[3] = glm::vec3{ x_, y_, z_ + 1 };
+  voxel_index_[4] = glm::vec3{ x_, y_ + 1, z_ };
+  voxel_index_[5] = glm::vec3{ x_ + 1, y_ + 1, z_ };
+  voxel_index_[6] = glm::vec3{ x_ + 1, y_ + 1, z_ + 1 };
+  voxel_index_[7] = glm::vec3{ x_, y_ + 1, z_ + 1 };
 };
 
 Isosurface::GridCell::~GridCell(){};
 
-glm::vec3 const&
-Isosurface::GridCell::VoxelIndex(int index) const
+glm::vec3 const& Isosurface::GridCell::operator[](int index) const
 {
   switch (index) {
     case 0:
@@ -338,7 +320,7 @@ Isosurface::GridCell::VoxelIndex(int index) const
     case 7:
       return voxel_index_[7];
     default:
-      fprintf(stderr, "Wrong edge index: %d\n", index);
+      fprintf(stderr, "Wrong gridcell index: %d\n", index);
       exit(EXIT_FAILURE);
   }
 }
