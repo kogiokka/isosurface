55b122d77a9558726e1c9c480c54c65da28479d9
Luis Kao <kogiokka.frms@gmail.com>
2023-03-08 03:14:37 +0800

    Improve CMake project structure

--- cmake/deps.cmake
+++
@@ -1,7 +0,0 @@
-include(FetchContent)
-
-include("${CMAKE_SOURCE_DIR}/cmake/deps/OpenGL.cmake")
-include("${CMAKE_SOURCE_DIR}/cmake/deps/glm.cmake")
-include("${CMAKE_SOURCE_DIR}/cmake/deps/SDL2.cmake")
-include("${CMAKE_SOURCE_DIR}/cmake/deps/glad.cmake")
-include("${CMAKE_SOURCE_DIR}/cmake/deps/imgui.cmake")
--- cmake/deps/glad.cmake
+++
@@ -1,3 +0,0 @@
-add_library(glad STATIC)
-target_sources(glad PRIVATE "${CMAKE_SOURCE_DIR}/extern/glad/src/glad.c")
-target_include_directories(glad PUBLIC "${CMAKE_SOURCE_DIR}/extern/glad/include")
